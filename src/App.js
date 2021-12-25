beae605b44c43b6fd857616399598af25bc17d82
조다혜 <joda0210@gmail.com>
2021-12-25 13:06:12 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css'
+import styled from 'styled-components'
+
+const Div = styled.div`
+  display: flex;
+  background-color: red;
+`
+
+function App() {
+  return <Div>rtr</Div>
+}
+
+export default App
